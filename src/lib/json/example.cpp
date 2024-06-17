1a5d77cbf1701eb060146edd1309d3dc5aed583d
ChenYingYin <chen15816602944@163.com>
2024-06-17 18:40:00 +0800

    add json example and spdlog

---
+++ src/lib/json/example.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+// ...
+int main() {
+  std::ifstream f("example.json");
+  if(f.is_open()) {
+    json data = json::parse(f);
+    json cap_cfg = data["capture"];
+    if(cap_cfg.is_array()) {
+      for(const auto& item : cap_cfg) {
+        std::string type = item["type"].template get<std::string>();
+        std::cout << type << std::endl;
+      }
+    }
+  }
+  return 0;
+}
